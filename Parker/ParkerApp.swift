f32b143dce28c56431c037afd0516bd2ea135281
Dennis Slavinsky <dsslavin@justin.tv>
2022-01-19 12:24:42 -0500

    Initial Commit

---
+++ Parker/ParkerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkerApp.swift
+//  Parker
+//
+//  Created by Slavinsky, Dennis on 1/19/22.
+//
+
+import SwiftUI
+
+@main
+struct ParkerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
